7d3c1ebc732908d46be53b3d2340ec8b7f098d87
JanickiSebastian <135833089+JanickiSebastian@users.noreply.github.com>
2023-07-18 22:50:03 +0200

    Add files via upload

---
+++ Projekt 1-JavaScript-Sebastian Janicki/js/outcomes/actions.js
@@ -0,0 +1,69 @@
+"use strict";
+import { outcomeName, outcomeValue } from "../main.js";
+import { renderOutcomesList } from "./updates.js";
+
+export let outcomes = [];
+
+export const addOutcome = (e) => {
+  e.preventDefault();
+
+  const nameValue = outcomeName.value.trim();
+  const valueValue = outcomeValue.value.trim();
+
+  if (
+    !nameValue ||
+    valueValue === "" ||
+    isNaN(valueValue) ||
+    Number(valueValue) <= 0
+  ) {
+    return;
+  }
+  const _outcome = {
+    name: outcomeName.value,
+    value: Number(outcomeValue.value),
+    id: Math.random().toString(),
+  };
+
+  outcomes.push(_outcome);
+
+  renderOutcomesList();
+
+  outcomeName.value = "";
+  outcomeValue.value = "";
+};
+
+export const deleteOutcome = (e) => {
+  e.preventDefault();
+  const idToDelete = e.target.id;
+  outcomes = outcomes.filter((el) => el.id !== idToDelete);
+
+  renderOutcomesList();
+};
+
+export const editOutcomesList = (e) => {
+  e.preventDefault();
+  const idToEdit = e.target.id.split("-")[2];
+  const nameValue = document.getElementById(`update-name-${idToEdit}`).value;
+  const outcomeValue = document.getElementById(
+    `update-outcome-${idToEdit}`
+  ).value;
+
+  if (
+    !nameValue ||
+    outcomeValue === "" ||
+    isNaN(outcomeValue) ||
+    Number(outcomeValue) <= 0
+  ) {
+    return;
+  }
+  {
+    outcomes = outcomes.map((outcome) => {
+      if (outcome.id === idToEdit) {
+        return { ...outcome, name: nameValue, value: Number(outcomeValue) };
+      }
+      return outcome;
+    });
+
+    renderOutcomesList();
+  }
+};
